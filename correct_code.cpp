e6e4394f070bf2d7ecd756b9fb61b5d356f501ed
新沛谭 <bobi20110114@outlook.com>
2025-06-11 20:55:59 +0800

    2801

--- correct_code.cpp
+++ correct_code.cpp
@@ -1,11 +1,113 @@
-
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
 #include <iostream>
-//这里写正确的代码的main函数前头的部分
+#include <stdint.h>
+#include <math.h>
+#include <cmath>
+using namespace std;
+
+const int INT_SIZE = sizeof(int);
+const int MAXN = 1000000 + 7;
+const int MAXBLOCK = 1000 + 7;
+
+int a[MAXN], d[MAXN], belong[MAXN];
+int L[MAXBLOCK], R[MAXBLOCK], lazy[MAXBLOCK];
+int n, q, block, tot, x, y, k;
+
+
+void build() {
+    block = (int)sqrt(n);
+    tot = (n + block - 1) / block;
+    for (int i = 1; i <= n; i++) {
+        belong[i] = (i - 1) / block + 1;
+    }
+    for (int i = 1; i <= tot; i++) {
+        L[i] = (i - 1) * block + 1;
+        R[i] = min(i * block, n);
+        sort(d + L[i], d + R[i] + 1);
+    }
+}
+
+void modify_part(int bid, int st, int ed) {
+    for (int i = st; i <= ed; i++) {
+        a[i] += k;
+    }
+    int len = R[bid] - L[bid] + 1;
+    memcpy(d + L[bid], a + L[bid], len * INT_SIZE);
+    sort(d + L[bid], d + R[bid] + 1);
+}
+
+void modify() {
+    if (belong[x] == belong[y]) {
+        modify_part(belong[x], x, y);
+        return;
+    }
+
+    modify_part(belong[x], x, R[belong[x]]);
+    modify_part(belong[y], L[belong[y]], y);
+
+    for (int i = belong[x] + 1; i < belong[y]; i++) {
+        lazy[i] += k;
+    }
+}
+
+int query_part(int bid, int st, int ed) {
+    int ret = 0;
+    for (int i = st; i <= ed; i++) {
+        if (a[i] + lazy[bid] >= k) {
+            ++ret;
+        }
+    }
+    return ret;
+}
+
+int query() {
+    if (belong[x] == belong[y]) {
+        return query_part(belong[x], x, y);
+    }
+
+    int ansL = query_part(belong[x], x, R[belong[x]]);
+    int ansR = query_part(belong[y], L[belong[y]], y);
+    int ansM = 0;
+
+    for (int i = belong[x] + 1; i < belong[y]; i++) {
+        int pos = lower_bound(d + L[i], d + R[i] + 1, k - lazy[i]) - d;
+        ansM += R[i] - pos + 1;
+    }
+
+    return ansL + ansR + ansM;
+}
 
 int main() {
-    
-    
-    //具体代码
-    puts("a");
+    freopen("input.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        d[i] = a[i];
+    }
+    build();
+    while (q--) {
+        char op;
+        cin >> op >> x >> y >> k;
+        switch (op) {
+        case 'M': {
+            modify();
+            break;
+        }
+        case 'A': {
+            cout << query() << '\n';
+            break;
+        }
+        }
+    }
     return 0;
 }
--- my_code.cpp
+++ my_code.cpp
@@ -1,9 +1,229 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
 #include <iostream>
-//这里写正确的代码的main函数前头的部分
-int main() {
-	
+#include <stdint.h>
+#include <math.h>
+#include <cmath>
 
-	//具体代码
+#define READ          false
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define MAXN          (size_t)(1e6 + 5)
+#define BLOCKS_SIZE   (size_t)(1e3+5)
 
-	return 0;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+//快读函数声明
+#if READ
+template< typename T >
+inline T readf();
+#else
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+#endif
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+struct Block {
+    std::vector<ll> vec;
+    ll tag;
+};
+
+Block blocks[BLOCKS_SIZE];
+size_t arr[MAXN], belong[MAXN];
+size_t blockSize, blockNum;
+ll n, q;
+
+inline void update(size_t index) {
+    for (size_t i = (index - 1) * 3 + 1, j = 0; j < blocks[index].vec.size(); j++, i++) {
+        blocks[index].vec[j] = arr[i];
+    }
+    std::sort(blocks[index].vec.begin(), blocks[index].vec.end());
 }
+
+inline void Add(size_t _First, size_t _Last, ll _Val) {
+    if (belong[_First] == belong[_Last]) {
+        for (size_t i = _First; i <= _Last; i++) {
+            arr[i] += _Val;
+        }
+        update(belong[_First]);
+    }
+    else {
+        for (size_t i = _First; i <= belong[_First] * blockSize; i++) {
+            arr[i] += _Val;
+        }
+        update(belong[_First]);
+        for (size_t i = belong[_First] + 1; i < belong[_Last]; i++) {
+            blocks[i].tag += _Val;
+        }
+        for (size_t i = (belong[_Last] - 1) * blockSize + 1; i <= _Last; i++) {
+            arr[i] += _Val;
+        }
+        update(belong[_Last]);
+    }
+    return;
+}
+
+inline ll query(size_t _First, size_t _Last, ll _Val) {
+    ll res = 0;
+    if (belong[_First] == belong[_Last]) {
+        for (size_t i = _First; i <= _Last; i++) {
+            res += (arr[i] >= _Val - blocks[belong[_First]].tag);
+        }
+    }
+    else {
+        for (size_t i = _First; i <= belong[_First] * blockSize; i++) {
+            res += (arr[i] >= _Val);
+        }
+        for (size_t i = belong[_First] + 1; i < belong[_Last]; i++) {
+            ll ans = blockSize;
+            for (size_t left = 0, right = blockSize - 1; left <= right; ) {
+                ll mid = (left + right) >> 1;
+                if (blocks[i].vec[mid] >= _Val - blocks[i].tag) {
+                    ans = mid;
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            res += blockSize - ans;
+        }
+        for (size_t i = (belong[_Last] - 1) * blockSize + 1; i <= _Last; i++) {
+            res += (arr[i] >= _Val - blocks[belong[_First]].tag);
+        }
+    }
+    return res;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    readf(&n), readf(&q);
+    blockSize = (int)(std::sqrt(n)) + 1;
+    blockNum = n / blockSize + (n % blockSize != 0);
+
+    for (size_t i = 1; i <= n; i++) {
+        readf(&arr[i]);
+        blocks[i / blockSize + 1].vec.push_back(arr[i]);
+        belong[i] = i / blockSize + 1;
+    }
+
+    for (size_t i = 1; i <= blockNum; i++) {
+        std::sort(blocks[i].vec.begin(), blocks[i].vec.end());
+        blocks[i].tag = 0;
+    }
+
+    while (q--) {
+        char opt[2];
+        scanf("%s", opt);
+        if (opt[0] == 'M') {
+            size_t first = readf<size_t>(), last = readf<size_t>();
+            ll val = readf<ll>();
+            Add(first, last, val);
+        }
+        else {
+            size_t first = readf<size_t>(), last = readf<size_t>();
+            ll val = readf<ll>();
+            printf("%lld\n", query(first, last, val));
+
+        }
+    }
+
+
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+#if READ
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+#else
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != NULL) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+#endif
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |  By txp2024     |  |  |     |         |      |
+ *      |  |                 |  |  |     |`---=    |      |
+ *      |  |  C:\>_          |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
