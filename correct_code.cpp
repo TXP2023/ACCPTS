24cfd1eec70a9c64bcd7331817a4f598e7341f79
新沛谭 <bobi20110114@outlook.com>
2025-06-11 20:50:42 +0800

    6.11

--- correct_code.cpp
+++ correct_code.cpp
@@ -3,8 +3,7 @@
 //这里写正确的代码的main函数前头的部分
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("ans.txt", "w", stdout);
+    
     
     //具体代码
     puts("a");
--- my_code.cpp
+++ my_code.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 //这里写正确的代码的main函数前头的部分
 int main() {
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	
 
 	//具体代码
 
