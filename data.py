8e5ea628fc2d07cee7d42891b7ee0209633216ab
user <user@E2>
2025-06-20 20:04:45 +0800

    1955

---
+++ data.py
@@ -0,0 +1,17 @@
+import random
+
+
+print(5)
+
+
+for _ in range(5):
+
+    n = random.randint(2, 10)
+    m = random.randint(2, 10)
+    
+
+    for __ in range(m):
+        opt = random.randint(0, 1)      
+        a = random.randint(1, n)        
+        b = random.randint(1, n)        
+        print(f"{opt} {a} {b}")
--- my_code.cpp
+++ my_code.cpp
@@ -1,9 +1,185 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <ctype.h>
+#include <cstdarg>
+#include <numeric>
+#include <climits>
+#include <time.h>
 #include <iostream>
-//这里写正确的代码的main函数前头的部分
-int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    //具体代码
+#include <stdint.h>
 
+#define READ          false
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define MAXN          (size_t)(1e5 + 5)
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+//快读函数声明
+#if READ
+template< typename T >
+inline T readf();
+#else
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+#endif
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+ll setEq[MAXN], setNe[MAXN];
+ll t, n, m;
+
+ll find(ll x, ll* _Set) {
+    if (_Set[x] == x) {
+        return x;
+    }
+    else {
+        return _Set[x] = find(_Set[x], _Set);
+    }
+}
+
+inline bool same_set(ll x, ll y, ll* _Set) {
+    return find(x, _Set) == find(y, _Set);
+}
+
+inline void merge(ll x, ll y, ll* _Set) {
+    _Set[find(x, _Set)] = _Set[find(y, _Set)];
+    return;
+}
+
+inline bool slove() {
+    //多种情况
+    // 让 u！=v
+    // 判断此时u？=v， 如果为true，那么就无法成立
+    //否则可以成立
+    //让 u==v
+    bool flag = true;//
+    readf(&n), readf(&m);
+    std::iota(setEq + 1, setEq + 1 + n, 1);
+    std::iota(setNe + 1, setNe + 1 + n, 1);
+    for (size_t i = 0; i < m; i++) {
+        ll opt, u, v;
+        if (!flag) {
+            continue;
+        }
+        readf(&opt), readf(&u), readf(&v);
+        if (opt == 1) {
+            if (same_set(u, v, setNe)) {
+                flag = false;
+            }
+            else {
+                merge(u, v, setEq);
+            }
+        }
+        else {
+            if (same_set(u, v, setEq)) {
+                flag = false;
+            }
+            else {
+                merge(u, v, setNe);
+            }
+        }
+    }
+    return flag;
+}
+
+int main() {
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    readf(&t);
+
+    while (t--) {
+        puts(slove() ? "YES" : "No");
+    }
+
+
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
     return 0;
 }
+
+#if READ
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+#else
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != NULL) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+#endif
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |  By txp2024     |  |  |     |         |      |
+ *      |  |                 |  |  |     |`---=    |      |
+ *      |  |  C:\>_          |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
