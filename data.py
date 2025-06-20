2ee4037e85b86aaa0c64bc4a195d6e29e21e3e86
user <user@E2>
2025-06-20 20:10:02 +0800

    1955

--- data.py
+++ data.py
@@ -5,13 +5,16 @@ print(5)
 
 
 for _ in range(5):
-
+    
     n = random.randint(2, 10)
     m = random.randint(2, 10)
     
-
+    
+    print(f"{n} {m}")
+    
+    
     for __ in range(m):
-        opt = random.randint(0, 1)      
-        a = random.randint(1, n)        
-        b = random.randint(1, n)        
+        opt = random.randint(0, 1)  
+        a = random.randint(1, n)    
+        b = random.randint(1, n)    
         print(f"{opt} {a} {b}")
